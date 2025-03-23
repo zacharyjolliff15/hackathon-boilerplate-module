5997c69e12d8651d7652e5a996b5d702bea8ab75
Austin West <austwest@umich.edu>
2025-03-23 00:11:19 -0400

    added test file

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 async function main() {
-  const genAI = new GoogleGenerativeAI("GEMINI_API_KEY");
+  const genAI = new GoogleGenerativeAI("AIzaSyAauA2p8okahW6ercDjloFvfJ98bqNX_0I");
   const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
   const prompt = "How does AI work?";
 
